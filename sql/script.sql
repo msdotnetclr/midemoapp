305c795dc2ab9dfea1167040e84eeddd7dd98b1b
Miracle Zhou <msdotnetclr@gmail.com>
2020-11-09 13:27:26 -0500

    add sql

---
+++ sql/script.sql
@@ -0,0 +1,4 @@
+﻿CREATE USER midemoapp FROM EXTERNAL PROVIDER 
+
+ALTER ROLE db_datareader ADD MEMBER midemoapp -- gives permission for normal app read to database
+ALTER ROLE db_datawriter ADD MEMBER midemoapp -- gives permission for normal app write to database
